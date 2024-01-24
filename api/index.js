4a65e284983254be382431de1b34ced7c3dbb63d
ahmed1995mansour <Ahmed1995mansour@gmail.com>
2024-01-24 11:36:24 +0200

    create a middleware to handle errors

--- api/index.js
+++ api/index.js
@@ -16,6 +16,15 @@ app.use(express.json())
 
 app.use('/api/auth', authRouter)
 app.use('/api/user', userRouter)
+app.use((error, req, res, next) => {
+    const statusCode = error.statusCode || 500;
+    const message = error.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
 
 app.listen(5005, ()=> {
 console.log('Server is listening on port 5005')
