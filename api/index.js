42c924e9e11cfc7d183a4d5043f54dbbe14adea7
ahmed1995mansour <Ahmed1995mansour@gmail.com>
2024-01-24 11:26:21 +0200

    create signup api endpoint

--- api/index.js
+++ api/index.js
@@ -1,15 +1,20 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import authRouter from './routes/auth.route.js'
 import userRouter from './routes/user.route.js'
+
 
 dotenv.config()
 mongoose.connect(process.env.MONGODB_URI).then(()=> {
     console.log('Connected to DB')
 }).catch((error) => console.log(error))
 
-const app = express()
+const app = express();
 
+app.use(express.json())
+
+app.use('/api/auth', authRouter)
 app.use('/api/user', userRouter)
 
 app.listen(5005, ()=> {
