423d9aaa39690d3a53ac99dcd16e21f5e03718af
ahmed1995mansour <Ahmed1995mansour@gmail.com>
2024-01-30 17:03:23 +0200

    add update user endpoint

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import authRouter from './routes/auth.route.js'
 import userRouter from './routes/user.route.js'
+import cookieParser from 'cookie-parser'
 
 
 dotenv.config()
@@ -12,7 +13,9 @@ mongoose.connect(process.env.MONGODB_URI).then(()=> {
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+app.use(cookieParser())
 
 app.use('/api/auth', authRouter)
 app.use('/api/user', userRouter)
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { hello } from '../controllers/user.controller.js';
+import { hello, updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router()
 
 router.get('/hello',hello)
 
+router.post('/update/:id', verifyToken ,updateUser)
 export default router
