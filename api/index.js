0e8b29dc3c5e9216a6cd9fb6b884bf4b3b04c4f5
ahmed1995mansour <Ahmed1995mansour@gmail.com>
2024-03-19 02:07:22 +0200

    add create listing endpoint

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import authRouter from './routes/auth.route.js'
 import userRouter from './routes/user.route.js'
+import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser'
 
 
@@ -19,6 +20,10 @@ app.use(cookieParser())
 
 app.use('/api/auth', authRouter)
 app.use('/api/user', userRouter)
+app.use('/api/listing', listingRouter);
+
+
+
 app.use((error, req, res, next) => {
     const statusCode = error.statusCode || 500;
     const message = error.message || 'Internal Server Error';
