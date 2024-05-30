efeaa626e883429b364febb9d97592fdf522ead0
ahmed1995mansour <Ahmed1995mansour@gmail.com>
2024-05-30 07:54:10 +0300

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
-
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About Find Estate</h1>
+      <p className="mb-4 text-slate-700">
+        Welcome to <span className="text-slate-500">Find</span>
+        <span className="text-slate-700">Estate</span> , where your real estate dreams become
+        reality. With years of experience and a passion for excellence, we specialize in providing
+        personalized, professional service to buyers, sellers, and investors. Our team of dedicated
+        experts is committed to guiding you through every step of the process, ensuring a seamless
+        and stress-free experience. Whether you're looking for your first home, seeking a lucrative
+        investment, or selling your property, we leverage our extensive market knowledge and
+        innovative strategies to achieve outstanding results. At{' '}
+        <span className="text-slate-500">Find</span>
+        <span className="text-slate-700">Estate</span>, we believe in building lasting relationships
+        based on trust, integrity, and exceptional service. Your success is our priority.
+      </p>
+      <p className="mb-4 text-slate-700">
+        we are dedicated to turning your real estate aspirations into reality. With a proven track
+        record and a deep understanding of the market, our team offers expert guidance and
+        personalized solutions for buying, selling, and investing in properties. We pride ourselves
+        on our commitment to integrity, professionalism, and customer satisfaction, ensuring that
+        each client receives tailored support throughout their real estate journey. From finding the
+        perfect home to maximizing investment opportunities, we utilize cutting-edge technology and
+        market insights to deliver exceptional results.
+      </p>
+      <p className="mb-4 text-slate-700">
+        your trusted partner in the world of real estate. Our mission is to provide unparalleled
+        service and expert guidance to help you navigate the complexities of buying, selling, and
+        investing in properties. With a team of experienced professionals and a deep commitment to
+        excellence, we tailor our approach to meet your unique needs and goals. From finding your
+        dream home to crafting strategic investment plans, we leverage our extensive market
+        knowledge and innovative tools to ensure your success.
+      </p>
+    </div>
+  );
 };
 
 export default About;
